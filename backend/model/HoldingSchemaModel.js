1137b3a5ada2db51aca58fb7491ee85c3edc27ce
Alex <alexgupta609@gmail.com>
2026-09-14 08:22:58 +0545

    Backend initialized

---
+++ backend/model/HoldingSchemaModel.js
@@ -0,0 +1,7 @@
+const {model} = require('mongoose');
+
+const {HoldingSchema} =require('../schema/HoldingSchema');
+
+const HoldingModel = model('Holding', HoldingSchema);
+
+module.exports = {HoldingModel};
